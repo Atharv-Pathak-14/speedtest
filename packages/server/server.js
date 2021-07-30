0ce14655744dace00fcf9e75b3f8fa5d84c23ba3
FatGrizzly <66728880+FatGrizzly@users.noreply.github.com>
2021-07-30 18:30:18 +0530

    Update server.js

--- packages/server/server.js
+++ packages/server/server.js
@@ -6,10 +6,6 @@ const HttpServer = require("./src/httpServer");
 
 const basePath = process.env.BASE_PATH || "web";
 const httpPort = process.env.PORT || 80;
-const httpsPort = process.env.PORT || 443;
-const key = process.env.KEY || "certificates/localhost.key";
-const cert = process.env.CERT || "certificates/localhost.pem";
-const ca = process.env.CA || undefined;
 const logger = new Logger();
 
 if (!fs.existsSync(path.join(process.cwd(), "results"))) {
@@ -20,4 +16,4 @@ process.on("SIGINT", function() {
   process.exit();
 });
 
-new HttpServer(basePath, httpPort, httpsPort, key, cert, ca);
+new HttpServer(basePath, httpPort);
