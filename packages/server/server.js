5a764d585c3aa69b4de0511dcf3c6b220ca435ad
Atharv Pathak <78468172+Atharv-Pathak-14@users.noreply.github.com>
2022-01-10 19:36:21 +0530

    Update server.js

--- packages/server/server.js
+++ packages/server/server.js
@@ -6,7 +6,7 @@ const HttpServer = require("./src/httpServer");
 
 const basePath = process.env.BASE_PATH || "web";
 const httpPort = process.env.PORT || 80;
-const httpsPort = 55543;
+const httpsPort = 433;
 const key = process.env.KEY || "certificates/localhost.key";
 const cert = process.env.CERT || "certificates/localhost.pem";
 const ca = process.env.CA || undefined;
